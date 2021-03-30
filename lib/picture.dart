dc7e5aff0744585541dd0c1df32dd8ab1af47144
Julio Gutierrez <bubuntux@gmail.com>
2021-03-29 23:53:07 -0500

    enable null safety

--- lib/picture.dart
+++ lib/picture.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/widgets.dart';
 
 class Picture {
-  const Picture(this.img, this.text, {this.value});
+  const Picture(this.img, this.text, this.value);
 
   final String img; //TODO image ?
   final String text;
-  final String? value;
+  final String value;
 }
 
 class PictureWidget extends StatelessWidget {
-  PictureWidget({this.pec}) : super(key: ObjectKey(pec));
+  PictureWidget(this.pec) : super(key: ObjectKey(pec));
 
   final Picture pec;
 
   @override
   Widget build(BuildContext context) {
-    Picture s= Picture("sdf", "sdf");
+    Picture s = Picture("sdf", "sdf", "sdf");
 
     return Container();
   }
