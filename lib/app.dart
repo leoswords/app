1573a31c9bc62e90388b1a9edeeff8c193010bb0
Julio Gutierrez <bubuntux@gmail.com>
2021-06-20 22:59:09 -0500

    wip

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts_web.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:leos_words/button.dart';
 import 'package:leos_words/page.dart' as p;
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'dart:io' show Platform;
-
-import 'package:flutter_tts/flutter_tts.dart';
 
 class App extends StatefulWidget {
   @override
@@ -13,8 +9,8 @@ class App extends StatefulWidget {
 }
 
 class _App extends State<App> {
+  final FlutterTts flutterTts = FlutterTts();
   String? _message;
-  late FlutterTts flutterTts;
 
   void _onTouch(String? msg) {
     setState(() {
@@ -23,87 +19,12 @@ class _App extends State<App> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    initTts();
-  }
-
-  TtsState ttsState = TtsState.stopped;
-
-  bool get isIOS => !kIsWeb && Platform.isIOS;
-
-  bool get isAndroid => !kIsWeb && Platform.isAndroid;
-
-  bool get isWeb => kIsWeb;
-
-  Future _getDefaultEngine() async {
-    var engine = await flutterTts.getDefaultEngine;
-    if (engine != null) {
-      print(engine);
-    }
-  }
-
-  initTts() {
-    flutterTts = FlutterTts();
-
-    if (isAndroid) {
-      _getDefaultEngine();
-    }
-
-    flutterTts.setStartHandler(() {
-      setState(() {
-        print("Playing");
-        ttsState = TtsState.playing;
-      });
-    });
-
-    flutterTts.setCompletionHandler(() {
-      setState(() {
-        print("Complete");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    flutterTts.setCancelHandler(() {
-      setState(() {
-        print("Cancel");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    if (isWeb || isIOS) {
-      flutterTts.setPauseHandler(() {
-        setState(() {
-          print("Paused");
-          ttsState = TtsState.paused;
-        });
-      });
-
-      flutterTts.setContinueHandler(() {
-        setState(() {
-          print("Continued");
-          ttsState = TtsState.continued;
-        });
-      });
-    }
-
-    flutterTts.setErrorHandler((msg) {
-      setState(() {
-        print("error: $msg");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    //flutterTts.setVolume(1.0);
-  }
-
-  @override
   Widget build(BuildContext context) {
     var bar = AppBar(
       toolbarHeight: 100,
       centerTitle: false,
       title: GestureDetector(
-        child: Text(_message ?? "empty"),
+        child: _message != null ? Text(_message!) : null,
         onTap: () => {
           if (_message != null) {flutterTts.speak(_message!)}
         },
--- lib/button.dart
+++ lib/button.dart
@@ -1,38 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Button {
-  Button({this.label, this.message, required this.onTouch});
-
-  String? label;
-  String? message;
-  OnTouch onTouch;
-}
-
 // Actions!
 typedef void OnTouch(String? message);
 
-class ButtonWidget extends StatelessWidget {
-  ButtonWidget(this._button) : super(key: ObjectKey(_button));
+class Button {
+  String label;
+  String message;
+  OnTouch onTouch;
 
+  Button({required this.label, message, required this.onTouch})
+      : this.message = message ?? label;
+}
+
+class ButtonWidget extends StatelessWidget {
   final Button _button;
+
+  ButtonWidget(this._button);
 
   @override
   Widget build(BuildContext context) {
-    var label = _button.label;
-    var child = label == null
-        ? SizedBox(
-            child: Container(),
-            height: 100,
-          )
-        : InkWell(
-            onTap: () => _button.onTouch(_button.message),
-            child: SizedBox(
-              height: 100,
-              child: Center(
-                child: Text(label),
-              ),
-            ));
-    return child;
+    return InkWell(
+        onTap: () => _button.onTouch(_button.message),
+        child: SizedBox(
+          height: 100,
+          child: Center(
+            child: Text(_button.label),
+          ),
+        ));
+    ;
   }
 }
